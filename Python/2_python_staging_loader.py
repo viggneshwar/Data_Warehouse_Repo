3c7f03a35252142cde129f19765126e695d9c4fb
Viggneshwar <viggneshwar@gmail.com>
2025-10-05 12:02:20 +0530

    Changes in the loader python

--- Python/2_python_staging_loader.py
+++ Python/2_python_staging_loader.py
@@ -3,12 +3,13 @@
 #1.Importing of Necessary Libraries
 import pandas as pd
 from sqlalchemy import create_engine
+import os
 
 #2. Defining necessary variables with values assigned
 username = 'we47user'
 password = "We47%40123" #rather than @ i am mentioning %40
 host = '34.63.107.222'
-db = 'test_w47'
+db = 'stgdb_viggneshwar'
 
 #3. Creating the DB connection using connection url using create_engine function & pymysql is used for providing driver to connect
 engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}:3306/{db}")
@@ -18,20 +19,24 @@ ds_folder="D:\\Learn\\Inceptez Data Engineering\\DW Project\\dataset\\"
 
 #5. Mapping of the source file with the target table using python dictionary
 files = {
-    "stg_accounts": ds_folder+"accounts.csv",
-    "stg_transactions": ds_folder+"transactions.csv",
-    "stg_payments": ds_folder+"payments.csv",
-    "stg_creditcard": ds_folder+"creditcard.csv",
-    "stg_loans": ds_folder+"loans.csv",
-    "stg_cust_profile": ds_folder+"cust.csv",
-    "stg_branches": ds_folder+"branches.csv",
-    "stg_employees": ds_folder+"employee.csv"
+    "stg_accounts": "accounts.csv",
+    "stg_transactions": "transactions.csv",
+    "stg_payments": "payments.csv",
+    "stg_creditcard": "creditcard.csv",
+    "stg_loans": "loans.csv",
+    "stg_cust_profile": "cust.csv",
+    "stg_branches": "branches.csv",
+    "stg_employees": "employee.csv"
 }
 
 #first iteration of for loop will assign table='stg_transactions' , file=folder+'transactions.csv'
 for table, file in files.items():
-    #read files from iteration and convert it into dataframe/memorytable
-    df = pd.read_csv(file)
-    #load the data from df/memorytable to actual db table
-    df.to_sql(table, con=engine, index=False, if_exists="replace")
-    print(f"Rows loaded in the table {table}")
+    if os.path.exists(ds_folder+file):
+        print(f"File {file} exists, proceeding to load data into table {table}.")
+        #read files from iteration and convert it into dataframe/memorytable
+        df = pd.read_csv(ds_folder+file)
+        #load the data from df/memorytable to actual db table
+        df.to_sql(table, con=engine, index=False, if_exists="replace")
+        print(f"Rows loaded in the table {table}")
+    else:
+        print(f"File {file} does not exist, skipping loading for table {table}.")
